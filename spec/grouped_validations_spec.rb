b167f1d6600f02ddaed7dd889cf94dbf53b6dfdb
Adam Meehan <adam.meehan@gmail.com>
2010-10-30 10:42:36 +1100

    refactor specs

--- spec/grouped_validations_spec.rb
+++ spec/grouped_validations_spec.rb
@@ -22,92 +22,82 @@ describe GroupedValidations do
     p.group_valid?(:dummy)
   end
 
-  it "should raise exception if valiation group not defined on group_valid check" do
-    p = Person.new
-    lambda { p.group_valid?(:dummy) }.should raise_exception
-  end
-
-  it "should run the validations defined inside the validation group" do
-    Person.validation_group :name do
-      validates_presence_of :first_name
-      validates_presence_of :last_name
-    end
-
-    p = Person.new
-    p.group_valid?(:name)
-    p.should have(2).errors
-
-    p.first_name = 'Dave'
-    p.last_name = 'Smith'
-    p.group_valid?(:name)
-    p.should have(0).errors
-  end
-
-  it "should run all validation groups passed to groups_valid?" do
-    Person.class_eval do
-      validation_group :first_name_group do
+  context ".group_valid?" do
+    it "should run the validations defined inside the validation group" do
+      Person.validation_group :name do
         validates_presence_of :first_name
-      end
-      validation_group :last_name_group do
-        validates_presence_of :last_name
-      end
-    end
-
-    p = Person.new
-    p.groups_valid?(:first_name_group, :last_name_group)
-    p.should have(2).errors
-  end
-
-  it "should run all validation including groups when valid? method called" do
-    Person.class_eval do
-      validation_group :first_name_group do
-        validates_presence_of :first_name
-      end
-      validation_group :last_name_group do
         validates_presence_of :last_name
       end
 
-      validates_presence_of :sex
+      p = Person.new
+      p.group_valid?(:name)
+      p.should have(2).errors
+
+      p.first_name = 'Dave'
+      p.last_name = 'Smith'
+      p.group_valid?(:name)
+      p.should have(0).errors
     end
 
-    p = Person.new
-    p.valid?
-    p.should have(3).errors
+    it "should raise exception if valiation group not defined" do
+      p = Person.new
+      lambda { p.group_valid?(:dummy) }.should raise_exception
+    end
+
+    it "should run all validation groups passed to groups_valid?" do
+      Person.class_eval do
+        validation_group :first_name_group do
+          validates_presence_of :first_name
+        end
+        validation_group :last_name_group do
+          validates_presence_of :last_name
+        end
+      end
+
+      p = Person.new
+      p.groups_valid?(:first_name_group, :last_name_group)
+      p.should have(2).errors
+    end
+
+    context "with validation context" do
+      it "should run only validations for explicit context" do
+        Person.validation_group :name do
+          validates_presence_of :last_name, :on => :update
+        end
+
+        p = Person.new
+        p.persisted = false
+        p.last_name = nil
+        p.group_valid?(:name, :context => :create)
+        p.should have(0).errors
+
+        p.persisted = true
+        p.group_valid?(:name, :context => :update)
+        p.should have(1).errors
+        p.last_name = 'Smith'
+        p.group_valid?(:name)
+        p.should have(0).errors
+      end
+    end
   end
 
-  it "should respect :on => :create validation context" do
-    Person.validation_group :name do
-      validates_presence_of :first_name, :on => :create
+  context ".valid?" do
+    it "should run all validation including groups when valid? method called" do
+      Person.class_eval do
+        validation_group :first_name_group do
+          validates_presence_of :first_name
+        end
+        validation_group :last_name_group do
+          validates_presence_of :last_name
+        end
+
+        validates_presence_of :sex
+      end
+
+      p = Person.new
+      p.valid?
+      p.should have(3).errors
     end
-
-    p = Person.new
-    p.group_valid?(:name, :context => :create)
-    p.should have(1).errors
-    p.first_name = 'Dave'
-    p.group_valid?(:name)
-    p.should have(0).errors
-
-    p.first_name = nil
-    p.group_valid?(:name, :context => :update)
-    p.should have(0).errors
-  end
-
-  it "should respect :on => :update validation context" do
-    Person.validation_group :name do
-      validates_presence_of :last_name, :on => :update
-    end
-
-    p = Person.new
-    p.last_name = nil
-    p.group_valid?(:name)
-    p.should have(0).errors
-
-    p.valid?.should be_true
-    p.group_valid?(:name, :context => :update)
-    p.should have(1).errors
-    p.last_name = 'Smith'
-    p.group_valid?(:name)
-    p.should have(0).errors
   end
 
   # Can no longer be done. Unless I find a work around.
